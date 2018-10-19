5a3895fe6e81708864e81e0141abb2d67216c729
unknown <laxmikanth.dhage@gmail.com>
2018-10-18 21:50:10 -0700

    Routing Introduction

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { HeroesComponent } from './heroes/heroes.component';
+
+const routes: Routes=[
+  { path: 'heroes', component: HeroesComponent }
+];
+
+@NgModule({
+  exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)]
+})
+export class AppRoutingModule { }
